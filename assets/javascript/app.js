548914f1529b98de289abe2bd6a1c6d93f4764de
Anthony Mitchell <j.ant.mitch@gmail.com>
2018-03-24 12:47:53 -0400

    updated JS

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,78 +5,81 @@ var newTopic = "";
 
 
 function makeButton() {
-    for (i = 0; i < topics.length; i++) { 
-    var newBtn = $("<button>");
-    newBtn.addClass("newBtn");
-    $(newBtn).addClass("btn-lg")
-    newBtn.attr("name", topics[i]);
-    newBtn.text(topics[i]);
-    $(".test").append(newBtn);
-    console.log(newBtn.attr("name"));
+    for (i = 0; i < topics.length; i++) {
+        var newBtn = $("<button>");
+        newBtn.addClass("newBtn");
+        $(newBtn).addClass("btn-lg")
+        newBtn.attr("name", topics[i]);
+        newBtn.text(topics[i]);
+        $(".test").append(newBtn);
+        console.log(newBtn.attr("name"));
     }
 };
 
 makeButton();
 
-$(document).on("click", ".newBtn", function() {
-    
+$(document).on("click", ".newBtn", function () {
+
     var topic = $(this).attr("name");
-       
+
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-      topic + "&api_key=HguWv52RtosH1F8yVBQz0foab2b0e32T&limit=10";
+        topic + "&api_key=HguWv52RtosH1F8yVBQz0foab2b0e32T&limit=10";
 
-      $(".test2").empty();
-    
+    $(".test2").empty();
+
     $.ajax({
-      url: queryURL,
-      method: "GET"
+        url: queryURL,
+        method: "GET"
     })
-      
-      .then(function(response) {
-        console.log(queryURL);
-        console.log(response);
-        
-        var results = response.data;
 
-        
+        .then(function (response) {
+            console.log(queryURL);
+            console.log(response);
+
+            var results = response.data;
+
+
             for (var i = 0; i < results.length; i++) {
                 var topicsDiv = $("<div>");
                 var topicsImage = $("<img>");
                 var p = $("<p>").text("Rating: " + results[i].rating);
-                topicsImage.attr("src", results[i].images.fixed_height_still.url);                
+                topicsImage.attr("src", results[i].images.fixed_height_still.url);
                 topicsImage.addClass("gif");
                 topicsImage.attr("true")
                 topicsDiv.append(topicsImage);
                 topicsDiv.append(p);
                 $(".test2").append(topicsDiv);
-                
-        }
 
-        // $(".gif").on("click", function() {
-        //         var state = $(this).attr("true");
+            }
 
-        // });
+            // $(".gif").on("click", function() {
+            //         var state = $(this).attr("true");
+
+            // });
 
 
-    
-    
+
+
+        });
+    // debugger
+
+  
+
 });
-// debugger
 
-$("#submit").on("click", function(){
+$(document).on("click", "#submit", function (event) {
     event.preventDefault();
-console.log("hey");
-    var newGif = $("input").val();
-// console.log(newGif)
+    // console.log("hey");
+    var newGif = $("#newGif").val();
+    console.log(newGif)
     var nextBtn = $("<button>");
     topics.push(newGif);
     // newBtn.text(topics);
+    $(".test").empty();
     makeButton();
-    
+
     // var test = $("<button>");
     // newGif.push(test);
 
     // console.log(topics);
 })
-
-});
